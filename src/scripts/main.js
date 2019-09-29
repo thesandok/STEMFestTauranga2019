5b66a88804fcdfa3e59f04e587b206295d0c9b46
Matias Iacono <matias.iacono@gmail.com>
2019-09-29 16:11:43 +1300

    Popup and messages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,7 +37,7 @@ AFRAME.registerComponent('registerevents', {
             w.localStorage.setItem("stemfest_storage", JSON.stringify(o));
         },
         getStoredObject: function () {
-            var result = JSON.parse(w.localStorage.getItem("stemfest_storage") || "{ }");
+            var result = JSON.parse(w.localStorage.getItem("stemfest_storage") || '{ "showPopup": true, "items": { } }');
             return result;
         }
     };
@@ -50,7 +50,9 @@ AFRAME.registerComponent('registerevents', {
 
         this._configuration = {
             panelButton: 'pin',
-            slider: 'slide-layer'
+            slider: 'slide-layer',
+            popup: 'popup',
+            popupOkButton: 'popupok'
         };
 
         this._isCollapsed = true;
@@ -58,8 +60,8 @@ AFRAME.registerComponent('registerevents', {
     };
 
     sar.prototype._loadStash = function (stash) {
-        for (var p in stash) {
-            if (stash.hasOwnProperty(p)) {
+        for (var p in stash.items) {
+            if (stash.items.hasOwnProperty(p)) {
                 d.getElementById('img-' + p).classList.remove('not-found');
             }
         }
@@ -67,16 +69,37 @@ AFRAME.registerComponent('registerevents', {
 
     sar.prototype._markerFound = function (markerId) {
         var data = storageManager.getStoredObject();
-        data[markerId] = true;
+        data.items[markerId] = true;
         storageManager.storeObject(data);
 
         this._loadStash(data);
+    };
+
+    sar.prototype._hidePopup = function () {
+        var data = storageManager.getStoredObject();
+        data.showPopup = false;
+        storageManager.storeObject(data);
+
+        d.getElementsByClassName(this._configuration.popup)[0].style.display = 'none';
+    };
+
+    sar.prototype._checkPopupVisibility = function () {
+        var data = storageManager.getStoredObject();
+        
+        if (data.showPopup === true) {
+            d.getElementsByClassName(this._configuration.popup)[0].style.display = 'inline';
+        }
     };
 
     sar.prototype.init = function () {
         var self = this;
         var pin = d.getElementsByClassName(this._configuration.panelButton)[0];
         var slider = d.getElementsByClassName(this._configuration.slider)[0];
+        var popupButton = d.getElementsByClassName(this._configuration.popupOkButton)[0];
+
+        popupButton.addEventListener('click', function () {
+            self._hidePopup();
+        });
 
         pin.addEventListener('click', function () {
             if (self._isCollapsed === true) {
@@ -88,7 +111,10 @@ AFRAME.registerComponent('registerevents', {
             self._isCollapsed = !self._isCollapsed;
         });
 
-        this._loadStash(storageManager.getStoredObject());
+        var storedData = storageManager.getStoredObject();
+
+        this._loadStash(storedData);
+        this._checkPopupVisibility();
     };
 
     w.StemFestAR = sar;
