8fddc2788f5be0342bf2b47f91760b023631ea51
Matias Iacono <matias.iacono@gmail.com>
2019-10-08 19:44:02 +1300

    Added nodejs serial arduino connector and client side for temp sensor

--- TempSensor/temperature_sensor/temperature_sensor.ino
+++ TempSensor/temperature_sensor/temperature_sensor.ino
@@ -5,9 +5,9 @@ dht DHT;
  
 void setup(){
  
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  //Serial.println("DHT11 Humidity & temperature Sensor\n\n");
   delay(1000);//Wait before accessing Sensor
  
 }//end "setup()"
@@ -17,12 +17,11 @@ void loop(){
  
     DHT.read11(dht_apin);
     
-    Serial.print("Current humidity = ");
-    Serial.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println("C  ");
+    //Serial.print("Current humidity = ");
+    Serial.println((String)DHT.humidity + "," + DHT.temperature);
+    //Serial.print("%  ");
+    //Serial.print("temperature = ");
+    //Serial.println("C  ");
     
     delay(5000);//Wait 5 seconds before accessing sensor again.
  
